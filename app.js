70423f447fa1ec03adfb7c98805bbd5bfe59689e
Priyanka Debnath <priyanka.neub@gmail.com>
2026-05-25 01:35:38 +0600

    1st commit

--- app.js
+++ app.js
@@ -1,1 +1,14 @@
-console.log("server running on localhost:500")
+import express from 'express';
+
+import{ PORT } from './config/env.js';
+
+const app = express();
+
+app.get('/',(req,res)=>{
+    res.send('welcome to the Subscription Tracker API!');
+})
+
+app.listen(PORT,()=>{
+    console.log(`subscription tracker API is running on http://localhost:${PORT}`);
+})
+export default app;
